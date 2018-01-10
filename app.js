843011db70f4ce2bb1ab713426ac57d024eb8049
Chintan Gandhi <chintanavailable@gmail.com>
2018-01-10 17:51:20 +0530

    added all files

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require("express"),
+bodyParser =require("body-parser"),
+http = require("http"),
+path = require("path"),
+cors = require("cors");
+
+var app = module.exports = express();
+
+app.set('pert',process.env.PORT || 8000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static(path.join(__dirname,"public")));
+app.use(cors());
+
+app.route("/").get(function(req,res){
+    res.sendFile(path.join(__dirname,"public","index.html"));
+});
+
+app.route("home").get(function(req,res){
+    res.sendFile(path.join(__dirname,"public","index.html"));
+});
+
+//start server
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
